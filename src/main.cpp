6c8ff84c47f022c2daa144ca8bd4e91ac06242aa
Dmitry <tdv@ngs.ru>
2016-08-29 01:11:21 +0300

    Finish work on TCPServer and start work on TCPClients

--- src/main.cpp
+++ src/main.cpp
@@ -3,6 +3,9 @@
 #include <memory>
 #include <vector>
 
+#include <thread>
+#include <chrono>
+
 #include <boost/asio.hpp>
 #include <boost/shared_array.hpp>
 #include <boost/thread.hpp>
@@ -155,6 +158,95 @@ namespace Mif
 
 namespace Mif
 {
+    namespace Common
+    {
+
+        class ThreadPool final
+        {
+        public:
+            using Task = std::function<void ()>;
+
+            ThreadPool(std::uint16_t count)
+                : m_work{new boost::asio::io_service::work{m_ioService}}
+            {
+                if (!count)
+                    throw std::invalid_argument{"[Mif::Common::ThreadPool] Thread count must be more than 0."};
+
+                std::exception_ptr exception{};
+                for ( ; count ; --count)
+                {
+                    m_threads.create_thread([this, &exception] ()
+                            {
+                                try
+                                {
+                                    m_ioService.run();
+                                }
+                                catch (std::exception const &e)
+                                {
+                                    exception = std::current_exception();
+                                    // TODO: to log
+                                    std::cerr << "Failed to run thread pool. Error: " << e.what() << std::endl;
+                                }
+                            }
+                        );
+
+                    if (exception)
+                        break;
+                }
+
+                if (exception)
+                {
+                    if (count)
+                        Stop();
+
+                    try
+                    {
+                        std::rethrow_exception(exception);
+                    }
+                    catch (std::exception const &e)
+                    {
+                        throw std::runtime_error{"[Mif::Common::ThreadPool] Failed to run thread pool. "
+                            "Error: " + std::string{e.what()}};
+                    }
+                }
+            }
+
+            ~ThreadPool()
+            {
+                Stop();
+            }
+
+            void Post(Task task)
+            {
+                m_ioService.post(task);
+            }
+
+        private:
+            boost::asio::io_service m_ioService;
+            std::unique_ptr<boost::asio::io_service::work> m_work;
+            boost::thread_group m_threads;
+
+            void Stop()
+            {
+                try
+                {
+                    m_work.reset();
+                    m_ioService.post([this] () { m_ioService.stop(); });
+                    m_threads.join_all();
+                }
+                catch (std::exception const &e)
+                {
+                    // TODO: to log
+                    std::cerr << "[Mif::Common::ThreadPool::Stop] Failed to stop thread pool. Error: " << e.what() << std::endl;
+                }
+            }
+        };
+
+    }   // namespace Common
+}   // namespace Mif
+
+namespace Mif
+{
     namespace Net
     {
 
@@ -166,23 +258,17 @@ namespace Mif
             virtual void CloseMe() = 0;
         };
 
-        using IControlPtr = std::shared_ptr<IControl>;
-
         struct IPublisher
         {
             virtual ~IPublisher() = default;
             virtual void Publish(Buffer buffer) = 0;
         };
 
-        using IPublisherPtr = std::shared_ptr<IPublisher>;
-
         struct ISubscriber
         {
             virtual ~ISubscriber() = default;
             virtual void OnData(Buffer buffer) = 0;
         };
-
-        using ISubscriberPtr = std::shared_ptr<ISubscriber>;
 
         struct ISubscriberFactory
         {
@@ -211,7 +297,17 @@ namespace Mif
             // ISubscriber
             virtual void OnData(Buffer buffer) override final
             {
-                throw std::runtime_error{"OnData not implemented."};
+                //std::cout << std::this_thread::get_id() << std::endl;
+                //std::cout << "Readed " << buffer.first << " bytes." << std::endl;
+                //throw std::runtime_error{"OnData not implemented."};
+                char const resp[] = "HTTP/1.x 200 OK\r\nContent-Length: 0\r\nConnection: Closed\r\n\r\n";
+                boost::shared_array<char> r(new char [sizeof(resp)]);
+                char *p = r.get();
+                char const *s = &resp[0];
+                while(*p++ = *s++);
+                Buffer b = std::make_pair(sizeof(resp) - 1, r);
+                m_publisher.lock()->Publish(b);
+                m_control.lock()->CloseMe();
             }
         };
 
@@ -244,10 +340,17 @@ namespace Mif
                 , public IControl
             {
             public:
-                TCPSession(boost::asio::ip::tcp::socket socket, ISubscriberFactory &factory)
+                TCPSession(boost::asio::ip::tcp::socket socket,
+                    Common::ThreadPool &workers, ISubscriberFactory &factory)
                     : m_socket(std::move(socket))
+                    , m_workers(workers)
                     , m_factory(factory)
                 {
+                    //std::cout << "TCPSession" << std::endl;
+                }
+                ~TCPSession()
+                {
+                    //std::cout << "~TCPSession" << std::endl;
                 }
 
                 void Start()
@@ -260,6 +363,7 @@ namespace Mif
 
             private:
                 boost::asio::ip::tcp::socket m_socket;
+                Common::ThreadPool &m_workers;
                 ISubscriberFactory &m_factory;
                 std::shared_ptr<ISubscriber> m_subscriber;
 
@@ -271,21 +375,21 @@ namespace Mif
                     {
                         auto self(shared_from_this());
 
-                        auto publisherTask = [self, buffer] ()
-                        {
-                            boost::asio::async_write(self->m_socket, boost::asio::buffer(buffer.second.get(), buffer.first),
-                                    [self, buffer] (boost::system::error_code error, std::size_t /*length*/)
-                                    {
-                                        if (error)
-                                        {
-                                            // TODO: error to log
-                                            self->CloseMe();
-                                        }
-                                    }
-                                );
-                        };
-
-                        m_socket.get_io_service().post(publisherTask);
+                        m_socket.get_io_service().post([self, buffer] ()
+                                {
+                                    boost::asio::async_write(self->m_socket, boost::asio::buffer(buffer.second.get(), buffer.first),
+                                            [self, buffer] (boost::system::error_code error, std::size_t /*length*/)
+                                            {
+                                                if (error)
+                                                {
+                                                    // TODO: error to log
+                                                    self->CloseMe();
+                                                    std::cout << "OnWrite error." << std::endl;
+                                                }
+                                            }
+                                        );
+                                }
+                            );
                     }
                     catch (std::exception const &)
                     {
@@ -298,15 +402,20 @@ namespace Mif
                 // IControl
                 virtual void CloseMe() override
                 {
-                    try
-                    {
-                        auto self = shared_from_this();
-                        m_socket.get_io_service().post([self] () { self->m_socket.close(); } );
-                    }
-                    catch (std::exception const &)
-                    {
-                        // TODO: to log
-                    }
+                    auto self = shared_from_this();
+                    m_socket.get_io_service().post([self] ()
+                            {
+                                try
+                                {
+                                    self->m_socket.close();
+                                }
+                                catch (std::exception const &e)
+                                {
+                                    std::cerr << "CloseMe error: " << e.what() << std::endl;
+                                    // TODO: to log
+                                }
+                            }
+                        );
                 }
 
                 //----------------------------------------------------------------------------
@@ -322,14 +431,34 @@ namespace Mif
                             try
                             {
                                 if (!error)
-                                    self->m_subscriber->OnData(std::move(buffer));
+                                {
+                                    self->m_workers.Post([self, length, buffer] ()
+                                            {
+                                                try
+                                                {
+                                                    self->m_subscriber->OnData(std::make_pair(length, std::move(buffer.second)));
+                                                }
+                                                catch (std::exception const &e)
+                                                {
+                                                    // TODO: to log
+                                                    std::cerr << "Failed to process data. Error: " << e.what() << std::endl;
+                                                }
+                                            }
+                                        );
+                                    self->DoRead();
+                                }
                                 else
+                                {
+                                    // TODO: to log
                                     self->CloseMe();
+                                    //std::cerr << "Failed to process data." << std::endl;
+                                }
                             }
-                            catch (std::exception const &)
+                            catch (std::exception const &e)
                             {
                                 // TODO: to lgg
                                 self->CloseMe();
+                                std::cerr << "Failed to process data. Error: " << e.what() << std::endl;
                             }
                         }
                     );
@@ -339,81 +468,126 @@ namespace Mif
             class TCPServer final
             {
             public:
-                TCPServer(std::string const &host, std::uint16_t port,
-                          std::uint16_t ioTreadCount,
-                          std::shared_ptr<ISubscriberFactory> factory)
-                    : m_factory(factory)
-                    , m_acceptor(m_ioService, boost::asio::ip::tcp::endpoint(
-                            boost::asio::ip::address::from_string(host), port)
-                        )
-                    , m_socket(m_ioService)
-                {
-                    std::unique_ptr<boost::asio::io_service::work> ioWork(new boost::asio::io_service::work(m_ioService));
-                    for (std::uint16_t i = 0 ; i < ioTreadCount ; ++i)
-                    {
-                        std::exception_ptr exception{};
-                        m_ioTreads.create_thread([this, &exception] ()
-                                {
-                                    try
-                                    {
-                                        m_ioService.run();
-                                    }
-                                    catch (std::exception const &)
-                                    {
-                                        // TODO: to log
-                                        exception = std::current_exception();
-                                    }
-                                }
-                            );
-                        if (exception)
-                        {
-                            m_socket.close();
-                            ioWork.reset();
-                            m_ioTreads.join_all();
-                            std::rethrow_exception(exception);
+                TCPServer(std::string const &host, std::string const &port,
+                    std::uint16_t workers, std::shared_ptr<ISubscriberFactory> factory)
+                try
+                    : m_factory{factory}
+                    , m_workers{std::make_shared<Common::ThreadPool>(workers)}
+                    , m_acceptor{m_ioService,
+                            [this, &host, &port] () -> boost::asio::ip::tcp::endpoint
+                            {
+                                boost::asio::ip::tcp::resolver resolver{m_ioService};
+                                boost::asio::ip::tcp::resolver::query query{host, port};
+                                return *resolver.resolve(query);
+                            } ()
                         }
-                        m_ioWork = std::move(ioWork);
+                    , m_socket{m_ioService}
+                    , m_work{m_ioService}
+                {
+                    std::exception_ptr exception{};
+                    m_thread.reset(new std::thread([this, &exception] ()
+                            {
+                                try
+                                {
+                                    m_ioService.run();
+                                }
+                                catch (std::exception const &e)
+                                {
+                                    exception = std::current_exception();
+                                }
+                            }
+                        )
+                    );
 
-                        DoAccept();
-                    }
+                    if (exception)
+                        std::rethrow_exception(exception);
+
+                    DoAccept();
+                }
+                catch (std::exception const &e)
+                {
+                    throw std::runtime_error{"[Mif::Net::Detail::TCPServer] Failed to start server. "
+                        "Error: " + std::string{e.what()}};
+                }
+                catch (...)
+                {
+                    throw std::runtime_error{"[Mif::Net::Detail::TCPServer] Failed to start server. Error: unknown."};
                 }
 
                 virtual ~TCPServer()
                 {
                     try
                     {
-                        m_socket.close();
-                        m_ioWork.reset();
-                        m_ioTreads.join_all();
+                        m_ioService.post([this] ()
+                                {
+                                    try
+                                    {
+                                        m_ioService.stop();
+                                    }
+                                    catch (std::exception const &e)
+                                    {
+                                        std::cerr << "[Mif::Net::Detail::~TCPServer] Failed to post 'stop' to server object. Error: " << e.what() << std::endl;
+                                    }
+                                }
+                            );
+
+                        m_thread->join();
                     }
-                    catch (std::exception const &)
+                    catch (std::exception const &e)
                     {
-                        // TODO: to log
+                        std::cerr << "[Mif::Net::Detail::~TCPServer] Failed to stop server. Error: " << e.what() << std::endl;
                     }
                 }
 
             private:
                 std::shared_ptr<ISubscriberFactory> m_factory;
+                std::shared_ptr<Common::ThreadPool> m_workers;
+                std::unique_ptr<std::thread> m_thread;
                 boost::asio::io_service m_ioService;
-                std::unique_ptr<boost::asio::io_service::work> m_ioWork;
                 boost::asio::ip::tcp::acceptor m_acceptor;
                 boost::asio::ip::tcp::socket m_socket;
-                boost::thread_group m_ioTreads;
+                boost::asio::io_service::work m_work;
 
                 void DoAccept()
                 {
                     m_acceptor.async_accept(m_socket,
                             [this] (boost::system::error_code error)
                             {
-                                if (!error)
+                                try
                                 {
-                                    std::make_shared<TCPSession>(std::move(m_socket), *m_factory)->Start();
+                                    if (!error)
+                                        std::make_shared<TCPSession>(std::move(m_socket), *m_workers, *m_factory)->Start();
+                                    else
+                                    {
+                                        std::cerr << "[Mif::Net::Detail::TCPServer::DoAccept] Failed tp accept connection." << std::endl;
+                                    }
+                                    DoAccept();
                                 }
-                                DoAccept();
+                                catch (std::exception const &e)
+                                {
+                                    std::cerr << "[Mif::Net::Detail::TCPServer::DoAccept] Failed to accept connection. Error: " << e.what() << std::endl;
+                                }
                             }
                         );
                 }
             };
+
+            class TCPClients final
+            {
+            public:
+                TCPClients()
+                {
+                    ;
+                }
+
+                ~TCPClients()
+                {
+                    ;
+                }
+
+            private:
+            };
+
         }   // namespace Detail
     }   // namespace Net
 }   // namespace Mif
@@ -423,7 +597,7 @@ int main()
     try
     {
         auto server = std::make_shared<Mif::Net::Detail::TCPServer>(
-            "127.0.0.1", 5555, 2, std::make_shared<Mif::Net::ClientFactory>());
+            "localhost", "5555", 4, std::make_shared<Mif::Net::ClientFactory>());
         std::cin.get();
     }
     catch (std::exception const &e)
