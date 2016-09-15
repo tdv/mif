199a81757f5a5ca95c119b404db5bb120586d7f6
tkachenko <tkachenko@corp.sputnik.ru>
2016-09-15 17:32:22 +0300

    Fix cmake script and add hello_world example

---
+++ examples/hello_world/server/src/hello_world_service.h
@@ -0,0 +1,31 @@
+#ifndef __HELLO_WORLD_SERVER_HELLO_WORLD_SERVICE_H__
+#define __HELLO_WORLD_SERVER_HELLO_WORLD_SERVICE_H__
+
+// STD
+#include <list>
+#include <mutex>
+
+// THIS
+#include "common/interface/ihello_world.h"
+
+class HelloWorldService final
+    : public IHelloWorld
+{
+public:
+    HelloWorldService();
+    ~HelloWorldService();
+
+private:
+    using LockType = std::mutex;
+    using LockGuard = std::lock_guard<LockType>;
+
+    mutable LockType m_lock;
+    std::list<std::string> m_words;
+
+    // IHelloWorld
+    virtual void AddWord(std::string const &word) override final;
+    virtual std::string GetText() const override final;
+    virtual void Clean() override final;
+};
+
+#endif  // !__HELLO_WORLD_SERVER_HELLO_WORLD_SERVICE_H__
