69863122c6d9323442e61a4f4f3b9a1b9a85a323
tdv <tdv@ngs.ru>
2017-08-15 00:19:36 +0300

    Added example http_echo_server

---
+++ examples/http_echo_server/src/main.cpp
@@ -0,0 +1,45 @@
+//-------------------------------------------------------------------
+//  MetaInfo Framework (MIF)
+//  https://github.com/tdv/mif
+//  Created:     08.2017
+//  Copyright (C) 2016-2017 tdv
+//-------------------------------------------------------------------
+
+// MIF
+#include <mif/application/http_server.h>
+#include <mif/common/log.h>
+#include <mif/net/http/constants.h>
+
+class Application
+    : public Mif::Application::HttpServer
+{
+public:
+    Application(int argc, char const **argv)
+        : HttpServer{argc, argv}
+    {
+    }
+
+private:
+    // Mif.Application.HttpService
+    virtual void Init(Mif::Net::Http::ServerHandlers &handlers) override final
+    {
+        handlers["/"] = [] (Mif::Net::Http::IInputPack const &request,
+                Mif::Net::Http::IOutputPack &response)
+        {
+            auto data = request.GetData();
+
+            MIF_LOG(Info) << "Process request \"" << request.GetPath() << request.GetQuery() << "\"\t Data: "
+                    << (data.empty() ? std::string{"null"} : std::string{std::begin(data), std::end(data)});
+
+            response.SetCode(Mif::Net::Http::Code::Ok);
+            response.SetHeader(Mif::Net::Http::Constants::Header::Connection::GetString(),
+                               Mif::Net::Http::Constants::Value::Connection::Close::GetString());
+            response.SetData(std::move(data));
+        };
+    }
+};
+
+int main(int argc, char const **argv)
+{
+    return Mif::Application::Run<Application>(argc, argv);
+}
