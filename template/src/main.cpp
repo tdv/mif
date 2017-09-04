1423eef316b7e043bb714c59b7183bcc9bb33eea
tdv <tdv@ngs.ru>
2017-09-04 22:36:32 +0300

    Fix app template

--- template/src/main.cpp
+++ template/src/main.cpp
@@ -1,4 +1,34 @@
-int main()
+// MIF
+#include <mif/application/application.h>
+#include <mif/common/log.h>
+
+class Application
+    : public Mif::Application::Application
 {
-    return 0;
+public:
+    Application(int argc, char const **argv)
+        : Mif::Application::Application{argc, argv}
+    {
+        MIF_LOG(Info) << "Application";
+    }
+    
+    ~Application()
+    {
+        MIF_LOG(Info) << "~Application";
+    }
+    
+    virtual void OnStart() override final
+    {
+        MIF_LOG(Info) << "OnStart";
+    }
+
+    virtual void OnStop() override final
+    {
+        MIF_LOG(Info) << "OnStop";
+    }
+};
+
+int main(int argc, char const **argv)
+{
+    return Mif::Application::Run<Application>(argc, argv);
 }
