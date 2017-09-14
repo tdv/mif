bb4c78c4fde10f6bcfdc4829fb3b222132730092
tdv <tdv@ngs.ru>
2017-09-14 23:23:22 +0300

    Continue

--- include/mif/orm/postgresql/schema.h
+++ include/mif/orm/postgresql/schema.h
@@ -12,8 +12,9 @@
 #include <string>
 
 // MIF
+#include "mif/common/static_string.h"
 #include "mif/orm/schema.h"
-#include "mif/orm/postgresql/detail/schema.h"
+#include "mif/reflection/reflection.h"
 
 namespace Mif
 {
@@ -21,6 +22,44 @@ namespace Mif
     {
         namespace PostgreSql
         {
+            namespace Detail
+            {
+
+                MIF_DECLARE_SRTING_PROVIDER(DefailtSchemaName, "public")
+
+                template <typename TName, typename ... TItems>
+                class Schema final
+                {
+                public:
+                    static std::string Dump()
+                    {
+                        std::string schemaName;
+                        std::string sql = CreateSchema<TName>(schemaName);
+                        return sql;
+                    }
+
+                private:
+                    template <typename T>
+                    static typename std::enable_if<std::is_same<T, DefailtSchemaName>::value, std::string>::type
+                    CreateSchema(std::string &name)
+                    {
+                        name = std::string{};
+                        return {};
+                    }
+
+                    template <typename T>
+                    static typename std::enable_if<!std::is_same<T, DefailtSchemaName>::value, std::string>::type
+                    CreateSchema(std::string &name)
+                    {
+                        name = T::GetString();
+                        std::string sql;
+                        sql = "CREATE SCHEMA " + name + ";\n\n";
+                        name += ".";
+                        return sql;
+                    }
+                };
+
+            }   // namespace Detail
 
             using DefailtSchemaName = Detail::DefailtSchemaName;
 
