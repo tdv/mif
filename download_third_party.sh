7c320497d14309a18d999816e39a4f74e5dd97dd
tdv <tdv@ngs.ru>
2018-01-06 22:00:37 +0300

    Switch third_party to boost 1.66.0

--- download_third_party.sh
+++ download_third_party.sh
@@ -4,10 +4,10 @@ git clone --branch v1.2.8 https://github.com/madler/zlib.git
 git clone --branch 1.8.0 https://github.com/open-source-parsers/jsoncpp.git
 git clone --branch release-2.1.8-stable https://github.com/libevent/libevent.git
 
-wget https://sourceforge.net/projects/boost/files/boost/1.65.1/boost_1_65_1.tar.gz
-tar zxvf boost_1_65_1.tar.gz
-mv boost_1_65_1 boost
-rm boost_1_65_1.tar.gz
+wget https://sourceforge.net/projects/boost/files/boost/1.66.0/boost_1_66_0.tar.gz
+tar zxvf boost_1_66_0.tar.gz
+mv boost_1_66_0 boost
+rm boost_1_66_0.tar.gz
 
 wget https://github.com/postgres/postgres/archive/REL9_6_2.tar.gz
 tar zxvf REL9_6_2.tar.gz
