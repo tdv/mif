3c3dfaaea5b860d22a9e298b2a67f77f24507a25
Dmitry <tdv@ngs.ru>
2020-05-07 15:27:19 +0300

    The metainfo for the ICheckable interface has been added.

---
+++ include/mif/remote/meta/service/icheckable.h
@@ -0,0 +1,34 @@
+//-------------------------------------------------------------------
+//  MetaInfo Framework (MIF)
+//  https://github.com/tdv/mif
+//  Created:     05.2020
+//  Copyright (C) 2016-2020 tdv
+//-------------------------------------------------------------------
+
+#ifndef __MIF_REMOTE_META_SERVICE_ICHECKABLE_H__
+#define __MIF_REMOTE_META_SERVICE_ICHECKABLE_H__
+
+// MIF
+#include "mif/remote/ps.h"
+#include "mif/service/icheckable.h"
+
+namespace Mif
+{
+    namespace Service
+    {
+        namespace Meta
+        {
+
+            using Service::ICheckable;
+
+            MIF_REMOTE_PS_BEGIN(ICheckable)
+                MIF_REMOTE_METHOD(IsGood)
+            MIF_REMOTE_PS_END()
+
+        }   // namespace Meta
+    }   // namespace Service
+}   // namespace Mif
+
+MIF_REMOTE_REGISTER_PS(Mif::Service::Meta::ICheckable)
+
+#endif  // !__MIF_REMOTE_META_SERVICE_ICHECKABLE_H__
