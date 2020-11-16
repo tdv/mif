c490f7306c5824377a33c1b753b594bee2890845
Dmitry Tkachenko <tdv@ngs.ru>
2020-11-17 01:32:10 +0300

    Update 3rdparty

--- download_third_party.sh
+++ download_third_party.sh
@@ -1,10 +1,10 @@
 mkdir third_party_sources ||:
 cd third_party_sources
-git clone --branch v1.2.8 https://github.com/madler/zlib.git
-git clone --branch 1.8.0 https://github.com/open-source-parsers/jsoncpp.git
-git clone --branch release-2.1.8-stable https://github.com/libevent/libevent.git
+git clone --branch v1.2.9 https://github.com/madler/zlib.git
+git clone --branch 1.9.4 https://github.com/open-source-parsers/jsoncpp.git
+git clone --branch release-2.1.12-stable https://github.com/libevent/libevent.git
 
-wget https://sourceforge.net/projects/boost/files/boost/1.66.0/boost_1_66_0.tar.gz
+wget https://dl.bintray.com/boostorg/release/1.66.0/source/boost_1_66_0.tar.gz
 tar zxvf boost_1_66_0.tar.gz
 mv boost_1_66_0 boost
 rm boost_1_66_0.tar.gz
