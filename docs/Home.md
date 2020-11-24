4c3ecdc2a158c75ae85f0758e25993bf66c74a58
Dmitry Tkachenko <tdv@ngs.ru>
2020-11-24 16:56:01 +0300

    Removes...

--- docs/Home.md
+++
@@ -1,2 +0,0 @@
-# MIF in C++
-[MIF is a C++ web-application backend framework.](https://github.com/tdv/mif/wiki/MIF.-Home-page#mif-is-a-c-web-application-backend-framework)
--- download_third_party.sh
+++ download_third_party.sh
@@ -2,7 +2,6 @@ mkdir third_party_sources ||:
 cd third_party_sources
 git clone --branch v1.2.9 https://github.com/madler/zlib.git
 git clone --branch 1.9.4 https://github.com/open-source-parsers/jsoncpp.git
-git clone --branch release-2.1.12-stable https://github.com/libevent/libevent.git
 
 wget https://dl.bintray.com/boostorg/release/1.74.0/source/boost_1_74_0.tar.gz
 tar zxvf boost_1_74_0.tar.gz
