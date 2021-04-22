c7a23f9e0f70b36dadc70f0260cd0b55d267ee03
Dmitry Tkachenko <tdv@ngs.ru>
2021-04-22 09:39:44 +0300

    Switch for using boost 1.76

--- download_third_party.sh
+++ download_third_party.sh
@@ -2,10 +2,10 @@ mkdir third_party_sources ||:
 cd third_party_sources
 git clone --branch v1.2.9 https://github.com/madler/zlib.git
 
-wget https://dl.bintray.com/boostorg/release/1.75.0/source/boost_1_75_0.tar.gz
-tar zxvf boost_1_75_0.tar.gz
-mv boost_1_75_0 boost
-rm boost_1_75_0.tar.gz
+wget https://dl.bintray.com/boostorg/release/1.76.0/source/boost_1_76_0.tar.gz
+tar zxvf boost_1_76_0.tar.gz
+mv boost_1_76_0 boost
+rm boost_1_76_0.tar.gz
 
 wget https://github.com/postgres/postgres/archive/REL9_6_2.tar.gz
 tar zxvf REL9_6_2.tar.gz
