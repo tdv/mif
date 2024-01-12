bd4f0b30ddf0bdbc9799a847b5859bf402c80827
Dmitry <tkachenkodmitryv@gmail.com>
2024-01-12 22:09:37 +0300

    Update versions for zlib and boost libraries (zlib: 1.3, boost: 1.84)

--- download_third_party.sh
+++ download_third_party.sh
@@ -1,11 +1,11 @@
 mkdir third_party_sources ||:
 cd third_party_sources
-git clone --branch v1.2.9 https://github.com/madler/zlib.git
+git clone --branch v1.3 https://github.com/madler/zlib.git
 
-wget https://boostorg.jfrog.io/artifactory/main/release/1.82.0/source/boost_1_82_0.tar.gz
-tar zxvf boost_1_82_0.tar.gz
-mv boost_1_82_0 boost
-rm boost_1_82_0.tar.gz
+wget https://boostorg.jfrog.io/artifactory/main/release/1.84.0/source/boost_1_84_0.tar.gz
+tar zxvf boost_1_84_0.tar.gz
+mv boost_1_84_0 boost
+rm boost_1_84_0.tar.gz
 
 wget https://github.com/postgres/postgres/archive/REL9_6_2.tar.gz
 tar zxvf REL9_6_2.tar.gz
